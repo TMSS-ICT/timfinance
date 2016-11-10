184185f1ba45c394fdc7c7564dc4102e6b5a0799
Rashida Sultana <rashida57pust@gmail.com>
2016-11-10 17:12:26 +0600

    sql add

--- timfinance_db.sql
+++ timfinance_db.sql
@@ -72,4 +72,133 @@ ALTER TABLE `users_groups`
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (1, 1, 1),
 (2, 2, 2);
+
+
+
+
+
+
+
+
+
+-- /genders table 
+CREATE TABLE IF NOT EXISTS `genders` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+INSERT INTO `genders` (`id`, `name`) VALUES
+(1, 'Male'),
+(2, 'FeMale');
+
+
+-- /zone table 
+CREATE TABLE IF NOT EXISTS `zones` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+INSERT INTO `zones` (`id`, `name`) VALUES
+(1, 'Dhaka');
+
+-- /areas table 
+CREATE TABLE IF NOT EXISTS `areas` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `zone_id` int(11) unsigned NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+ALTER TABLE `areas`
+  ADD CONSTRAINT `fk_zone_id` FOREIGN KEY (`zone_id`) REFERENCES `zones` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+INSERT INTO `areas` (`id`, `zone_id`, `name`) VALUES
+(1, 1, "Gazipur");
+
+-- /branches table 
+CREATE TABLE IF NOT EXISTS `branches` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `area_id` int(11) unsigned NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+ALTER TABLE `branches`
+  ADD CONSTRAINT `fk_area_id` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+INSERT INTO `branches` (`id`, `area_id`, `name`) VALUES
+(1, 1, "Kapasia");
+-- /groups table 
+CREATE TABLE IF NOT EXISTS `m_groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `branch_id` int(11) unsigned NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+ALTER TABLE `m_groups`
+  ADD CONSTRAINT `fk_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+INSERT INTO `m_groups` (`id`, `branch_id`, `name`) VALUES
+(1, 1, "Ranigong");
+
+CREATE TABLE IF NOT EXISTS `m_groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `branch_id` int(11) unsigned NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;   
+ALTER TABLE `m_groups`
+  ADD CONSTRAINT `fk_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+INSERT INTO `m_groups` (`id`, `branch_id`, `name`) VALUES
+(1, 1, "Ranigong");
+
+-- //member table
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `zone_id` int(11) unsigned NOT NULL,
+  `area_id` int(11) unsigned NOT NULL,
+  `branch_id` int(11) unsigned NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `sur_name` varchar(100) NOT NULL,
+  `gender_id` int(11) unsigned NOT NULL,
+  `age` int(11) unsigned NOT NULL,
+  `education_id` int(11) unsigned NOT NULL,
+  `passing_year` int(11) unsigned NOT NULL,
+
+
+  `f_first_name` varchar(40) DEFAULT NULL,
+  `f_last_name` varchar(40) DEFAULT NULL,
+  `f_sur_name` varchar(40) DEFAULT NULL,
+  `f_age` int(11) unsigned NOT NULL,
+  `f_business` varchar(40) DEFAULT NULL,
+  `m_first_name` varchar(40) DEFAULT NULL,
+  `m_last_name` varchar(40) DEFAULT NULL,
+  `m_sur_name` varchar(40) DEFAULT NULL,
+  `m_age` varchar(40) DEFAULT NULL,
+
+
+   
+  `vill_name` varchar(40) DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+ 
+  `company` varchar(100) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `account_status_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_laccount_status1` FOREIGN KEY (`account_status_id`) REFERENCES `account_status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`,   `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `account_status_id`, `first_name`, `last_name`, `company`, `mobile`) VALUES
+(1, '\0\0', 'superadmin', '59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4', '9462e8eee0', 'admin@admin.com', NULL, NULL, NULL, 1268889823, 1373438882, 1, 'Admin', 'admin', 'ADMIN', '0'),
+(2, '\0\0', 'member', '59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4', '9462e8eee0', 'member@member.com', NULL, NULL, NULL, 1268889823, 1373438882, 1, 'Member', 'Member', 'MEMBER', '0');
+
+
+
+
+
+
+
+
+
+
+
   
